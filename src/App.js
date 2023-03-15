2dce87d574afa376594adfd34613b5c3f2a9d18f
Robx33 <robsonc25@outlook.com>
2023-03-15 15:44:56 -0300

    comentários

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import QRCode from 'react-qr-code'
-import QRcodeLink from 'qrcode'
+// Biblioteca QRCode
+import QRCode from 'react-qr-code' // Imagem do QRCode
+import QRcodeLink from 'qrcode' // Link para baixar
 
 import { useState } from 'react';
 
 function App() {
 
-const [link, setLink] = useState('')
-const [qrcodeLink, setQrcodeLink] = useState('')
+const [link, setLink] = useState('') // Estado para armazenar o que o usuário digita e o valor do QRcode
+const [qrcodeLink, setQrcodeLink] = useState('') // Estado que recebe o link qrcode digitado
 
-function handleGenerate(link_url){
-  QRcodeLink.toDataURL(link_url, {
+// Função para baixar o QRCode
+function handleGenerate(link_url){ // Espera o link que vai ser gerado
+  QRcodeLink.toDataURL(link_url, { // Recebe o link
     width: 600,
     margin: 3,
-  }, function(err, url){
+  }, function(err, url){ // Função de callback
     setQrcodeLink(url)
   })
 }
@@ -36,7 +38,7 @@ function handleQRcode(e){
       onChange={(e) => handleQRcode(e)}
       />
 
-      <a href={qrcodeLink} download={`qrcode.png`}>Baixar QRCode</a>
+      <a href={qrcodeLink} download={`qrcode.png`}>Baixar QRCode</a> 
 
     </div>
   );
